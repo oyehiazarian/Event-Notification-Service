f12d148dbf3ef06fd4be4b85a7c687740ab43dbb
Oleksandr <oyehiazarian@united-internet.de>
2025-04-26 22:15:54 +0200

    feat: logic with new evnt for frontend

--- client-service/src/components/home/Home.jsx
+++ client-service/src/components/home/Home.jsx
@@ -1,10 +1,119 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';  // Подключаем CSS файл
 
 const Home = () => {
+  const [showForm, setShowForm] = useState(false);  // Стейт для отображения формы
+  const [formData, setFormData] = useState({
+    title: '',
+    topic: '',
+    content: '',
+    due_date: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+
+    const token = localStorage.getItem("token"); // Извлекаем токен из localStorage
+
+    if (!token) {
+      alert("Вы не авторизованы!");
+      return;
+    }
+    e.preventDefault();
+    fetch('http://localhost:8080/new_event', {
+      method: 'POST',
+    
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        id: 0,
+        title: formData.title,
+        topic: formData.topic,
+        content: formData.content,
+        due_date: "",
+        userId: {}
+       
+      }),
+    })  
+      .then((data) => {
+        alert('New task added');
+        setShowForm(false);  // Скрыть форму после отправки
+        setFormData({
+          title: '',
+          topic: '',
+          content: '',
+          due_date: '',
+        }); // Очистить поля
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+
+
+        alert('Failed to add new task');
+      });
+    }
+
   return (
     <div className="container">
-      <button className="button">New Task</button>
+      {!showForm ? (
+        <button className="button" onClick={() => setShowForm(true)}>
+          New Task
+        </button>
+      ) : (
+        <form className="task-form" onSubmit={handleSubmit}>
+          <div className="form-field">
+            <label>Title</label>
+            <input
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-field">
+            <label>Topic</label>
+            <input
+              type="text"
+              name="topic"
+              value={formData.topic}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-field">
+            <label>Content</label>
+            <textarea
+              name="content"
+              value={formData.content}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+          <div className="form-field">
+            <label>Due Date</label>
+            <input
+              type="date"
+              name="due_date"
+              value={formData.due_date}
+              onChange={handleChange}
+              
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
 };
