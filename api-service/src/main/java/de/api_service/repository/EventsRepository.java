4006c05ccf806c162d2a7279d680f182eaff74ae
Oleksandr <oyehiazarian@united-internet.de>
2025-11-04 10:52:17 +0100

    chore: update project

---
+++ api-service/src/main/java/de/api_service/repository/EventsRepository.java
@@ -0,0 +1,15 @@
+package de.api_service.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import de.api_service.model.Events;
+import de.api_service.model.User;
+
+@Repository
+public interface EventsRepository extends JpaRepository<Events, Long> {
+    List<Events> findAllByUserId(User userId);
+
+    List<Events> findByIdGreaterThan(Long lastId);
+}
---
+++ api-service/src/main/java/de/api_service/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package de.api_service.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import de.api_service.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
